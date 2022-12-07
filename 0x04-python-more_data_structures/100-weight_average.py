df792f5c7960c2e695048572619a5035756fbfa7
Fanyafulu <fanyafulu@yahoo.com>
2022-12-07 15:55:10 -0800

    task

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def weight_average(my_list=[]):
     if not isinstance(my_list, list) or len(my_list) == 0:
         return (0)
@@ -8,5 +9,4 @@ def weight_average(my_list=[]):
     for tup in my_list:
         avg += (tup[0] * tup[1])
         size += tup[1]
-
-        return (avg / size)
+    return (avg / size)
